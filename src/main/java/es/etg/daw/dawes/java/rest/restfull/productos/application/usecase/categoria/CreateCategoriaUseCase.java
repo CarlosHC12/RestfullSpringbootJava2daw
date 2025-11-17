8ab784c77b249c297cb2d276f74ee4d1983438f4
Carlos Hurtado <carloshurtadocortes@gmail.com>
2025-11-17 22:42:21 +0100

    ljkbwejrf

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/categoria/CreateCategoriaUseCase.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/categoria/CreateCategoriaUseCase.java
@@ -3,23 +3,22 @@ package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase.catego
 import java.time.LocalDateTime;
 
 import es.etg.daw.dawes.java.rest.restfull.productos.application.command.categoria.CreateCategoriaCommand;
-import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Producto;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.model.Categoria;
 import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.CategoriaRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+
+@RequiredArgsConstructor
 public class CreateCategoriaUseCase {
-    
-    private CategoriaRepository categoriaRepository;
 
-    public Producto create( CreateCategoriaCommand comando){
+    private final CategoriaRepository categoriaRepository;
 
-        Producto categoria = Producto.builder() // Se puede usar comando.id y no getId por usar @Accessors(fluent = true) la clase CreateProductoCommand 
-                                    .nombre(comando.nombre())
-                                    .createdAt(LocalDateTime.now()).build();
-
+    public Categoria create(CreateCategoriaCommand comando){
+        Categoria categoria = Categoria.builder()           
+                              .nombre(comando.nombre())
+                              .createdAt(LocalDateTime.now()).build();
         categoriaRepository.save(categoria);
         return categoria;
-
     }
 }
+
